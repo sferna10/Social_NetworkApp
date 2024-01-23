819996c3ecdd486a72d14a593a836d8639fab0e8
Silvia A. Fernandez <139423719+sferna10@users.noreply.github.com>
2024-01-23 09:10:13 -0500

    debugging

--- Main/models/Thought.js
+++ Main/models/Thought.js
@@ -16,7 +16,7 @@ const thoughtSchema = new Schema(
     // get: timestamp => dateFormat(timestamp)
    },   
    username: {
-    Type: String,
+    type: String,
     required: true,
     },
     reactions: [reactionSchema]   
